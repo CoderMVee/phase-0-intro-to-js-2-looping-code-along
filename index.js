8093c05fecd7966fa863f9f8b0febc354fe4d731
Marcelle Vukoja <booksread555@gmail.com>
2022-11-20 14:30:27 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+/*function writeCards(arrayNames, event) {
+    let thankYouCards = []
+    for (let i = 0; i < arrayNames.length; i++) {
+        thankYouCards.push(`"Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards;        
+}*/
+
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+        debugger;
+    }
+    console.log(startingNumber);
+}
+countDown(10)
